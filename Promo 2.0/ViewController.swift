7190ef281f1cd15aada74fa51c3702123b4da47b
Kobe Clifton <Kobecod@gmail.com>
2022-04-06 15:05:24 -0400

    Initial Commit

---
+++ Promo 2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Promo 2.0
+//
+//  Created by MBP-21 on 4/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
